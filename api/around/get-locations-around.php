d1f2278c6c2465f0a9e715d26cbb4ec760c53c22
Zasenko <dmitry.zasenko@gmail.com>
2023-11-27 17:47:51 +0100

    api around // when places not found

--- api/around/get-locations-around.php
+++ api/around/get-locations-around.php
@@ -24,7 +24,11 @@ require_once('../dbconfig.php');
 $places = array();
 $events = array();
 
-$sql = "SELECT id, name, type_id, avatar, main_photo, address, latitude, longitude, tags, timetable, is_active, updated_at FROM Place WHERE SQRT(POW(latitude - ?, 2) + POW(longitude - ?, 2)) <= 10 ";
+$sql = "SELECT
+    id, name, type_id, avatar, main_photo, address, latitude, longitude, tags, timetable, is_active, updated_at
+FROM
+    Place
+WHERE SQRT(POW(latitude - ?, 2) + POW(longitude - ?, 2)) * 111.32 <= 20";
 $params = [$float_latitude, $float_longitude];
 $types = "dd";
 $stmt = executeQuery($conn, $sql, $params, $types);
@@ -59,7 +63,44 @@ while ($row = $result->fetch_assoc()) {
     array_push($places, $place);
 }
 
-$sql = "SELECT id, name, type_id, country_id, region_id, city_id, latitude, longitude, start_date, start_time, finish_date, finish_time, address, location, poster, poster_small, is_free, tags, place_id, is_active, updated_at FROM Event WHERE SQRT(POW(latitude - ?, 2) + POW(longitude - ?, 2)) <= 20";
+if (empty($places)) {
+    $sql = "SELECT id, name, type_id, avatar, main_photo, address, latitude, longitude, tags, timetable, is_active, updated_at, SQRT(POW(latitude - ?, 2) + POW(longitude - ?, 2)) * 111.32 AS distance FROM Place ORDER BY distance ASC LIMIT 5";
+    $params = [$float_latitude, $float_longitude];
+    $types = "dd";
+    $stmt = executeQuery($conn, $sql, $params, $types);
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    while ($row = $result->fetch_assoc()) {
+        $is_active = (bool)$row['is_active'];
+        $tags = json_decode($row['tags'], true);
+        $timetable = json_decode($row['timetable'], true);
+
+        $avatar = $row['avatar'];
+        $avatar_url = isset($avatar) ? "https://www.navigay.me/" . $avatar : null;
+
+        $main_photo = $row['main_photo'];
+        $main_photo_url = isset($main_photo) ? "https://www.navigay.me/" . $main_photo : null;
+
+        $place = array(
+            'id' => $row['id'],
+            'name' => $row["name"],
+            'type_id' => $row['type_id'],
+            'avatar' => $avatar_url,
+            'main_photo' => $main_photo_url,
+            'address' => $row['address'],
+            'latitude' => $row['latitude'],
+            'longitude' => $row['longitude'],
+            'tags' => $tags,
+            'timetable' => $timetable,
+            'is_active' => $is_active,
+            'updated_at' => $row['updated_at'],
+        );
+        array_push($places, $place);
+    }
+}
+
+$sql = "SELECT id, name, type_id, country_id, region_id, city_id, latitude, longitude, start_date, start_time, finish_date, finish_time, address, location, poster, poster_small, is_free, tags, place_id, is_active, updated_at FROM Event WHERE SQRT(POW(latitude - ?, 2) + POW(longitude - ?, 2)) * 111.32 <= 20";
 $params = [$float_latitude, $float_longitude];
 $types = "dd";
 $stmt = executeQuery($conn, $sql, $params, $types);
