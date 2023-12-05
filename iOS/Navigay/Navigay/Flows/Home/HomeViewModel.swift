7dfa7c65dd391e4dffae5ab5ff1ebcaaa22adbda
Zasenko <dmitry.zasenko@gmail.com>
2023-12-05 16:20:23 +0100

    fix HomeView - events filtering

--- iOS/Navigay/Navigay/Flows/Home/HomeViewModel.swift
+++ iOS/Navigay/Navigay/Flows/Home/HomeViewModel.swift
@@ -1,18 +1,18 @@
 //
-//  LoginView.swift
+//  HomeViewModel.swift
 //  Navigay
 //
-//  Created by Dmitry Zasenko on 03.10.23.
+//  Created by Dmitry Zasenko on 28.11.23.
 //
 
 import SwiftUI
 
-struct LoginView: View {
+struct HomeViewModel: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LoginView()
+    HomeViewModel()
 }
