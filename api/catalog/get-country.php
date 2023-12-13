a002c48e5ef9831019da492c6ad1b01263c7c3b2
Zasenko <dmitry.zasenko@gmail.com>
2023-12-13 15:25:46 +0100

    api
    add get-country.php

---
+++ api/catalog/get-country.php
@@ -0,0 +1,133 @@
+<?php
+
+require_once('../error-handler.php');
+require_once('../languages.php');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    sendError('Invalid request method.');
+}
+
+if (!isset($_GET["id"]) || !is_numeric($_GET["id"])) {
+    sendError('Invalid or missing "id" parameter.');
+}
+$country_id = (int)$_GET["id"];
+
+$language = isset($_GET['language']) && in_array($_GET['language'], $languages) ? $_GET['language'] : 'en';
+
+
+require_once('../dbconfig.php');
+
+$sql = "SELECT id, isoCountryCode, name_$language, about, flag_emoji, photo, show_regions, is_active, updated_at FROM Country WHERE id = ?";
+
+$params = [$country_id];
+$types = "i";
+$stmt = executeQuery($conn, $sql, $params, $types);
+$result = $stmt->get_result();
+$stmt->close();
+$row = $result->fetch_assoc();
+
+//about
+$about_data = json_decode($row['about'], true);
+$selected_language_data = null;
+$eng_language_data = null;
+$any_language_data = null;
+if (is_array($about_data)) {
+    foreach ($about_data as $aboutItem) {
+        if ($aboutItem['language'] === $language) {
+            $selected_language_data = $aboutItem['about'];
+            break;
+        } else if ($aboutItem['language'] === 'en') {
+            $eng_language_data = $aboutItem['about'];
+            break;
+        }
+        $any_language_data = $aboutItem['about'];
+    }
+}
+$about = $selected_language_data ?? $eng_language_data ?? $any_language_data;
+//show_regions
+$show_regions = (bool)$row['show_regions'];
+//is_active
+$is_active = (bool)$row['is_active'];
+//photo
+$photo = $row['photo'];
+$photo_url = isset($photo) ? "https://www.navigay.me/" . $photo : null;
+
+$country = array(
+    'id' => $row['id'],
+    'isoCountryCode' => $row['isoCountryCode'],
+    'name' => $row["name_$language"],
+    'flag_emoji' => $row['flag_emoji'],
+    'photo' => $photo_url,
+    'about' => $about,
+    'show_regions' => $show_regions,
+    'is_active' => $is_active,
+    'updated_at' => $row['updated_at']
+);
+
+$sql = "SELECT id, name_$language, photo, is_active, updated_at FROM Region WHERE country_id = ?";
+$params = [$country_id];
+$types = "i";
+$stmt = executeQuery($conn, $sql, $params, $types);
+$result = $stmt->get_result();
+$stmt->close();
+
+$regions = array();
+while ($row = $result->fetch_assoc()) {
+
+    //is_active
+    $is_active = (bool)$row['is_active'];
+    //photo
+    $photo = $row['photo'];
+    $photo_url = isset($photo) ? "https://www.navigay.me/" . $photo : null;
+    //id
+    $region_id = $row['id'];
+
+    $region = array(
+        'id' => $region_id,
+        'name' => $row["name_$language"],
+        'photo' => $photo_url,
+        'is_active' => $is_active,
+        'updated_at' => $row['updated_at']
+    );
+
+    $sql = "SELECT id, name_$language, photo, is_active, updated_at FROM City WHERE region_id = ?";
+
+    $params = [$region_id];
+    $types = "i";
+    $stmt = executeQuery($conn, $sql, $params, $types);
+    $result = $stmt->get_result();
+    $stmt->close();
+
+    $cities = array();
+    while ($row = $result->fetch_assoc()) {
+
+        //is_active
+        $is_active = (bool)$row['is_active'];
+        //photo
+        $photo = $row['photo'];
+        $photo_url = isset($photo) ? "https://www.navigay.me/" . $photo : null;
+
+        $city = array(
+            'id' => $row['id'],
+            'name' => $row["name_$language"],
+            'photo' => $photo_url,
+            'is_active' => $is_active,
+            'updated_at' => $row['updated_at']
+        );
+        array_push($cities, $city);
+    }
+    $region += ['cities' => $cities];
+    array_push($regions, $region);
+
+    //TODO
+    // if (count($cities) > 0) {
+    //     array_push($regions, $region);
+    // } 
+}
+
+$country += ['regions' => $regions];
+
+$conn->close();
+$json = array('result' => true, 'country' => $country);
+echo json_encode($json, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE);
+exit;
