eaae7c1b462d25eb443d641f314d3209885d8069
Zasenko <dmitry.zasenko@gmail.com>
2023-12-13 12:22:12 +0100

    api
    add get-countries method

---
+++ api/catalog/get-countries.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once('../error-handler.php');
+require_once('../languages.php');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    sendError('Invalid request method.');
+}
+
+$language = isset($_GET['language']) && in_array($_GET['language'], $languages) ? $_GET['language'] : 'en';
+
+require_once('../dbconfig.php');
+
+$sql = "SELECT id, isoCountryCode, name_$language, flag_emoji, photo, show_regions, is_active, updated_at FROM Country";
+
+$stmt = $conn->prepare($sql);
+if (!$stmt) {
+    $conn->close();
+    sendError('Failed to prepare SQL statement: ' . $error);
+}
+if (!$stmt->execute()) {
+    $error = $stmt->error;
+    $stmt->close();
+    $conn->close();
+    sendError('Execute error: ' . $error);
+}
+$result = $stmt->get_result();
+$stmt->close();
+
+$countries = array();
+while ($row = $result->fetch_assoc()) {
+
+    $show_regions = (bool)$row['show_regions'];
+    $is_active = (bool)$row['is_active'];
+
+    $photo = $row['photo'];
+    $photo_url = isset($photo) ? "https://www.navigay.me/" . $photo : null;
+
+    $country = array(
+        'id' => $row['id'],
+        'isoCountryCode' => $row['isoCountryCode'],
+        'name' => $row["name_$language"],
+        'flag_emoji' => $row['flag_emoji'],
+        'photo' => $photo_url,
+        'show_regions' => $show_regions,
+        'is_active' => $is_active,
+        'updated_at' => $row['updated_at']
+    );
+    array_push($countries, $country);
+}
+$conn->close();
+$json = array('result' => true, 'countries' => $countries);
+echo json_encode($json, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE);
