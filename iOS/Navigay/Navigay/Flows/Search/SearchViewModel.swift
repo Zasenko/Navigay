43111a2161e547641c32a35c43dfeb50e7d02e75
Zasenko <dmitry.zasenko@gmail.com>
2023-12-24 12:28:08 +0100

    create SearchViewModel.swift
    move SearchViewModel from SearchView to SearchViewModel.swift

--- iOS/Navigay/Navigay/Flows/Search/SearchViewModel.swift
+++ iOS/Navigay/Navigay/Flows/Search/SearchViewModel.swift
@@ -6,13 +6,57 @@
 //
 
 import SwiftUI
+import SwiftData
 
-struct SearchViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension SearchView {
+    @Observable
+    class SearchViewModel {
+        
+        var modelContext: ModelContext
+        
+        var countries: [Country] = []
+        
+        var searchText: String = ""
+        
+        let catalogNetworkManager: CatalogNetworkManagerProtocol
+        let placeNetworkManager: PlaceNetworkManagerProtocol
+        let eventNetworkManager: EventNetworkManagerProtocol
+        
+        init(modelContext: ModelContext, catalogNetworkManager: CatalogNetworkManagerProtocol, placeNetworkManager: PlaceNetworkManagerProtocol, eventNetworkManager: EventNetworkManagerProtocol) {
+            self.modelContext = modelContext
+            self.catalogNetworkManager = catalogNetworkManager
+            self.eventNetworkManager = eventNetworkManager
+            self.placeNetworkManager = placeNetworkManager
+        }
+        
+        func fetch() {
+            if !catalogNetworkManager.isCountriesLoaded {
+                Task {
+                    guard let decodedCountries = await catalogNetworkManager.fetchCountries() else {
+                        return
+                    }
+                    await MainActor.run {
+                        for decodedCountry in decodedCountries {
+                            if let country = countries.first(where: { $0.id == decodedCountry.id} ) {
+                                country.updateCountryIncomplete(decodedCountry: decodedCountry)
+                            } else if decodedCountry.isActive {
+                                let country = Country(decodedCountry: decodedCountry)
+                                modelContext.insert(country)
+                                countries.append(country)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        func getCountriesFromDB() {
+            do {
+                let descriptor = FetchDescriptor<Country>(sortBy: [SortDescriptor(\.name)])
+                countries = try modelContext.fetch(descriptor)
+            } catch {
+                debugPrint(error)
+            }
+        }
     }
-}
-
-#Preview {
-    SearchViewModel()
 }
