1c1251755b42090b26b356d4a80c8314c2f91630
Zasenko <dmitry.zasenko@gmail.com>
2023-12-27 12:36:29 +0100

    api: fix place/add-comment.php & place/get-comments.php

--- api/place/add-comment.php
+++ api/place/add-comment.php
@@ -16,6 +16,9 @@ $place_id = $decodedComment["place_id"];
 $user_id = $decodedComment["user_id"];
 $comment = $decodedComment["comment"];
 $rating = $decodedComment["rating"];
+
+// TODO!!!! USER ID проверить в базе!
+
 if (!isset($place_id) || !isset($user_id)) {
     sendError('place id and user id is required.');
 }
@@ -24,14 +27,14 @@ $rating = !isset($rating) ? intval($rating) : null;
 require_once('../dbconfig.php');
 
 $sql = "INSERT INTO PlaceComment (place_id, user_id, comment, rating) VALUES (?, ?, ?, ?)";
-$params = [$name, $type_id, $country_id, $region_id, $city_id, $address, $latitude, $longitude, $email, $phone, $www, $facebook, $instagram, $about, $tags, $timetable, $other_info, $owner_id, $is_active, $is_checked];
+$params = [$place_id, $user_id, $comment, $rating];
 $types = "iisi";
 $stmt = executeQuery($conn, $sql, $params, $types);
 
 if (checkInsertResult($stmt, $conn, 'Failed to insert data into Place table.')) {
     $comment_id = getLastInsertId($conn);
     $conn->close();
-    $json = ['result' => true, 'comment_id' => $comment_id];
+    $json = ['result' => true];
     echo json_encode($json, JSON_NUMERIC_CHECK);
     exit;
 }
--- api/place/get-comments.php
+++ api/place/get-comments.php
@@ -16,8 +16,9 @@ if ($place_id <= 0) {
 
 require_once('../dbconfig.php');
 
-$sql = "SELECT pc.id, pc.comment, pc.rating, pc.photos, pc.created_at, pc.is_active, pcr.id AS reply_id, pcr.comment AS reply_text, pcr.created_at AS reply_created_at, pc.is_active AS reply_is_active
+$sql = "SELECT pc.id, pc.comment, pc.rating, pc.photos, pc.created_at, pc.is_active, u.id AS user_id, u.name AS user_name, u.bio AS user_bio, u.photo AS user_photo, pcr.id AS reply_id, pcr.comment AS reply_text, pcr.created_at AS reply_created_at, pc.is_active AS reply_is_active
 FROM PlaceComment pc
+    LEFT JOIN User u ON pc.user_id = u.id
     LEFT JOIN PlaceCommentReply pcr ON pc.id = pcr.comment_id
     WHERE pc.place_id = ?
     ORDER BY pc.created_at DESC, pcr.created_at ASC
@@ -43,7 +44,6 @@ while ($row = $result->fetch_assoc()) {
             array_push($photos_urls, $url);
         }
     }
-
     $comment = array(
         "id" => $row['id'],
         'comment' => $row["comment"],
@@ -52,15 +52,29 @@ while ($row = $result->fetch_assoc()) {
         'created_at' => $row['created_at'],
         'is_active' => $is_active,
     );
-
+    $reply_id = $row['reply_id'];
+    $reply_text = $row['reply_id'];
+    $reply_created_at = $row['reply_created_at'];
     $reply_is_active = (bool)$row['reply_is_active'];
-    $reply = array(
-        "id" => $row['reply_id'],
-        'comment' => $row["reply_text"],
-        'created_at' => $row['reply_created_at'],
-        'is_active' => $reply_is_active,
+    if (isset($reply_id) || isset($reply_text) || isset($reply_created_at)) {
+        $reply = array(
+            "id" => $reply_id,
+            'comment' => $reply_text,
+            'created_at' => $reply_created_at,
+            'is_active' => $reply_is_active,
+        );
+        $comment += ['reply' => $reply];
+    }
+    $user_photo = $row['user_photo'];
+    $user_photo_url = isset($user_photo) ? "https://www.navigay.me/" . $user_photo : null;
+
+    $user = array(
+        "id" => $row['user_id'],
+        'bio' => $row["user_bio"],
+        'name' => $row["user_name"],
+        'photo' => $user_photo_url
     );
-    $comment += ['reply' => $reply];
+    $comment += ['user' => $user];
     array_push($comments, $comment);
 }
 $conn->close();
